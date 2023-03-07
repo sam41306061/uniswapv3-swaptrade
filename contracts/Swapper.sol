21d1777b5e2920ee1b8711086a70f9734e68ece4
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2023-03-07 05:59:11 -0700

    inital setup of swap contract, boiler plate

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -9,5 +9,35 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // --> https://docs.uniswap.org/contracts/v3/guides/swaps/single-swaps
 
 contract Swapper {
-    // Code goes here...
+    address public owner; // person who calls it. 
+    address public immutable SWAP_ROUTER; //state variable
+
+    // way to call router without hardcoded router addresses 
+    constructor(address _SWAP_ROUTER){
+        SWAP_ROUTER = _SWAP_ROUTER;
+        owner = msg.sender;
+    }
+
+    // inital function 
+    function swap(
+        address[] memory _path,
+        uint24 _fee,
+        uint256 _amountIn // how much we want to swap
+    ) public{
+        // build perams
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: 'address1',
+                tokenOut: 'address2',
+                fee: _fee,
+                recipient: address(this), // can use msg.sender here to
+                deadline: block.timestamp,
+                amountIn: _amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        // swap
+        ISwapRouter(SWAP_ROUTER).exactInputSingle(params);
+        //transfer crypto
+    }
 }
